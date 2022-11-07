209febcb334cae6a81fc2ab841693c11cacd72ba
Eric Yates <k.ericyates@gmail.com>
2022-11-07 11:41:10 -0500

    able to read file

--- cryptoMagic.c
+++ cryptoMagic.c
@@ -8,29 +8,36 @@ decrypt();
 
 int main (int argc, char *argv[]){ //argc for arguement count argv for arguement vector  * is  pointer
 
-/*There will be three arguements.  The program itself, the file to be changed, and to encrypt or decrypt*/
+/*There will be three arguements. [0] The program itself,[1] the file to be changed, [2]and to encrypt or decrypt*/
 
 if (argc ==2){
         printf("Insufficient arguments.  Please supply file and select -D or -E.\n");
 }
 else if (argc==3){
     printf("Correct number of arguements supplied.\nThe file %s will be %s.\n", argv[1],(*argv[2]=='D')?"decrypted":"encrypted"); //can't select decrypted.  has to do with char variable.
-    printf("%s",argv[2]);
+    printf("argv[2]: %s\n",argv[2]);
 
-    if (*argv[2]=='D'){
+    if (*argv[2]=='D'){ //Try *argv2
         printf("\nRun decrypt function");
     }
-    else
-        printf("\nRun encrypt function"); //UNTIL fIX ARGUMENT SWITCH THIS RUNS
+    else{
+            
+        FILE *pf; //pf pointer file
+        char data[120];//20 lines 120 characters each
+        pf = fopen(argv[1],"r"); //open file function, r for read mode
         
-        /*
-        FILE *pf;
-        char data[50];
-        pf = fopen("text.txt","r"); //open file function, r for read mode?
-        fgets(data, 50,pf);
-        printf("It read %s",data);
-        */
+        while(!feof(pf)){ //while end of file not reached
+            //int line =0;
+            fgets(data, 120,pf);
+            //line++;
+            printf("It read %s\n",data);
 
+        }
+        
+        
+    }
+    
+        
 
 
 
